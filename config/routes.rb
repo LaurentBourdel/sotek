b8e02ae09eb047a0a41b8502d577e64833421494
LaurentBourdel <solartech971@gmail.com>
2021-10-29 10:19:25 -0400

    p

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+ 
+ 	devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+  end
+
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
